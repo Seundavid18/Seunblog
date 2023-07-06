d293e36148b8ae5e8157f6e0c488af498062bd7e
Seun-David <oyewumidavid84@gmail.com>
2023-07-06 23:45:37 +0100

    resturctured files, updated production script and deploy to azure

--- server.js
+++ server.js
@@ -46,7 +46,11 @@ app.use('/api/categories', catRouter)
 //     res.sendFile(path.join(__dirname, "/client/build", "index.html"))
 // })
 
+app.use(express.static("./client/build"))
 
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
