44f3fe51c2c967db8b5dfdbb354731b0a48bf8df
Seundavid18 <oyewumidavid84@gmail.com>
2024-04-27 15:56:21 +0100

    updated app.js

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use('/api/categories', catRouter)
 //     res.sendFile(path.join(__dirname, "/client/build", "index.html"))
 // })
 
-app.use(express.static("/client/build"))
+app.use(express.static("../client/build"))
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
