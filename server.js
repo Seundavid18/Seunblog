f09558b63a793c1d0c0c57d94e3dbeeb78814fd8
Seundavid18 <oyewumidavid84@gmail.com>
2024-04-28 12:46:19 +0100

    updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 
 connectDB();
 app.use(express.json())
-app.use("/images", express.static(path.join(__dirname, "/images")))
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const PORT = process.env.PORT || 8080
 
@@ -22,45 +22,31 @@ const storage = multer.diskStorage({
     filename:(req, file, cb) => {
         cb(null, req.body.name)
     },
-})
+});
 
 const upload = multer({storage: storage})
 app.post('/api/upload', upload.single("file"), (req, res) => {
     res.status(200).json('File has been uploaded')
-})
+});
 
 const authRouter = require("./src/routes/auth");
-const userRouter = require("./src/routes/user")
-const postRouter = require("./src/routes/post")
-const catRouter = require("./src/routes/category")
+const userRouter = require("./src/routes/user");
+const postRouter = require("./src/routes/post");
+const catRouter = require("./src/routes/category");
 
 app.use(cors())
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/posts', postRouter);
-app.use('/api/categories', catRouter)
+app.use('/api/categories', catRouter);
 
-// app.use(express.static(path.join(__dirname, "/client/build")))
-
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/client/build", "index.html"))
-// })
-
-app.use(express.static("./client/build"))
+app.use(express.static("./client/build"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-})
-
-// const buildPath = path.join(__dirname, '../client/build')
-
-// app.use(express.static(buildPath))
-
-// app.get("/*", (req, res) => {
-//     res.sendFile('index.html', { root : buildPath })
-// })
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
-})
+});
 
